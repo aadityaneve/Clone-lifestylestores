c064d7a0c1036a7e00cf881a069506860bc879ef
Aditya Neve <aditya.neve@gmail.com>
2021-10-20 20:00:09 +0530

    form validations added in checkout and signup page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,14 +17,14 @@ basket.forEach(function (element) {
 
     let details = document.createElement('h4');
     details.innerText = element.category;
-    
+
     let price = document.createElement('h4');
     price.innerText = "₹ " + element.price;
     let hr = document.createElement('hr');
     imgDiv.append(img);
     detailsDiv.append(brand, details, price);
     div.append(imgDiv, detailsDiv);
-    checkoutProductsDiv.append(div,hr);
+    checkoutProductsDiv.append(div, hr);
 });
 
 let totalAmount = 0;
@@ -44,9 +44,64 @@ checkoutTotalDiv.append(checkoutTotal);
 
 // pay now button direct to success.html
 let payNowBtn = document.getElementById('payNow');
-payNowBtn.addEventListener('click', function () {
-    localStorage.removeItem("basket");
-    window.location.href = 'success.html';
+payNowBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Currently working on  ----
+    let isFormValid = true;
+
+    // Validations for checkout form
+    let form = document.getElementById('checkoutForm');
+
+    if (form.fullName.value[0] == " " || form.fullName.value.length < 4) {
+        isFormValid = false;
+        alert("Enter valid name.");
+    } else if (form.fullName.value[0] == " " || isNaN(form.mobileNumber.value)) {
+        isFormValid = false;
+        alert("Mobile number cant be character or symbol.");
+    } else if (form.mobileNumber.value[0] == " " || form.mobileNumber.value.length != 10) {
+        isFormValid = false;
+        alert("Enter 10 digit mobile number.");
+    } else if (form.pincode.value[0] == " " || isNaN(form.pincode.value)) {
+        isFormValid = false;
+        alert("Pin Code cant be character or symbol.");
+    } else if (form.pincode.value[0] == " " || form.pincode.value.length != 6) {
+        isFormValid = false;
+        alert("Enter 6 digit pin code.");
+    } else if (form.city.value[0] == " " || form.city.value.length < 3) {
+        isFormValid = false;
+        alert("Enter valid city.");
+    } else if (form.state.value[0] == " " || form.state.value.length < 4) {
+        isFormValid = false;
+        alert("Enter valid state.");
+    } else if (form.buildingName.value[0] == " " || form.buildingName.value.length < 2) {
+        isFormValid = false;
+        alert("Enter valid building name.");
+    } else if (form.street.value[0] == " " || form.street.value.length < 4) {
+        isFormValid = false;
+        console.log(form.street.value.length);
+        alert("Enter valid street.");
+    } else if (form.cardNumber.value[0] == " " || form.cardNumber.value.length != 16) {
+        isFormValid = false;
+        alert("Enter 16 digit card number.");
+    } else if (form.nameOnCard.value[0] == " " || form.nameOnCard.value.length < 4) {
+        isFormValid = false;
+        alert("Enter valid name.");
+    } else {
+
+        let year = Number(form.expiryDate.value.slice(0, 4));
+        let month = Number(form.expiryDate.value.slice(5, 7));
+
+        if (year < new Date().getFullYear() || month < new Date().getMonth()) {
+            isFormValid = false;
+            alert("Expiry date is not valid.");
+        }
+    }
+
+    if (isFormValid === true) {
+        localStorage.removeItem("basket");
+        window.location.href = 'success.html';
+    }
 });
 
 
--- scripts/signup-or-signin-mob.js
+++ scripts/signup-or-signin-mob.js
@@ -1,6 +1,6 @@
 
 
-if(localStorage.getItem('userLoginDetails') === null){
+if (localStorage.getItem('userLoginDetails') === null) {
     localStorage.setItem('userLoginDetails', JSON.stringify([]));
 }
 
@@ -8,7 +8,7 @@ if(localStorage.getItem('userLoginDetails') === null){
 
 let continueBtn = document.getElementById('continueBtn');
 
-continueBtn.addEventListener('click', function(e){
+continueBtn.addEventListener('click', function (e) {
     e.preventDefault();
 
     let userLoginDetails = JSON.parse(localStorage.getItem('userLoginDetails'));
@@ -16,6 +16,7 @@ continueBtn.addEventListener('click', function(e){
     let mobileNumber = document.getElementById('mobileNumber').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
+    let isFormValid = true;
 
     let userDetails = {
         mobile: mobileNumber,
@@ -23,14 +24,23 @@ continueBtn.addEventListener('click', function(e){
         password: password
     };
 
-    userLoginDetails.push(userDetails);
 
-    localStorage.setItem('userLoginDetails',JSON.stringify(userLoginDetails));
+    if (mobileNumber[0] == " " || mobileNumber == "") {
+        isFormValid = false;
+        alert("Enter valid mobile number.");
+    } else if (email[0] == " " || email == "") {
+        isFormValid = false;
+        alert("Enter valid email.");
+    } else if (password.length < 6 || password.length > 15) {
+        isFormValid = false;
+        alert("Password should be between 6 - 15 characters.");;
+    }
 
-    if(mobileNumber != "" && email != "" && password != ""){
+
+    if (isFormValid) {
+        userLoginDetails.push(userDetails);
+        localStorage.setItem('userLoginDetails', JSON.stringify(userLoginDetails));
         window.location.href = 'signin-with-email.html';
-    }else{
-        alert("All fields are mandatory.");
     }
 });
 
